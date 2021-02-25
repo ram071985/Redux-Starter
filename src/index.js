837e13a1c9e161dbe9bba8d0151f9f1fc91a4190
ram071985 <reidmuchow@gmail.com>
2021-02-25 11:01:44 -0600

    added lodash dependancy. Removed wrapInDiv, and added lodash function for result.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+import { transform } from 'lodash';
+import { compose, pipe } from 'lodash/fp';
+
 let input = "   JavaScript   ";
 let output = "<div>" + input.trim() + "</div";
 
 const trim = str => str.trim();
-const wrapInDiv = str => `<div>${str}</div>`;
+const wrap = (type, str) => `<${type}>${str}</${type}>`;
 const toLowerCase = str => str.toLowerCase();
+
+const transform = pipe(trim, toLowerCase, wrap);
+transform(input);
 
 const result = wrapInDiv(toLowerCase(trim(input)));
