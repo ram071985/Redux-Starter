664976a555418392e6bf98a54e82f8e92379bdd5
ram071985 <reidmuchow@gmail.com>
2021-03-04 17:27:58 -0600

    added createSlice function from redux toolkit. Removed createAction functions.

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,25 +1,27 @@
 // Action types
-import { createAction, createReducer } from "@reduxjs/toolkit";
-
-// Action creators
-export const bugAdded = createAction("bugAdded");
-export const bugRemoved = createAction("bugRemoved");
-export const bugResolved = createAction("bugResolved");
-
-// Reducer
+import { createSlice } from "@reduxjs/toolkit";
 let lastId = 0;
 
-export default createReducer([], {
-  [bugAdded.type]: (bugs, action) => {
-    bugs.push({
-      id: ++lastId,
-      description: action.payload.description,
-      resolved: false,
-    });
-  },
+const slice = createSlice({
+  name: "bugs",
+  initialState: [],
+  reducers: {
+    bugAdded: (bugs, action) => {
+      bugs.push({
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false,
+      });
+    },
 
-  [bugResolved.type]: (bugs, action) => {
-   const index = bugs.findIndex(bug => bug.id === action.payload.id)
-   bugs[index].resolved = true;
-  }
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+      bugs[index].resolved = true;
+    },
+  },
 });
+
+export const {bugAdded, bugResolved} = slice.actions;
+export default slice.reducer;
+
+
